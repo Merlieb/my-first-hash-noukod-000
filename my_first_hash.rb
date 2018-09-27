bd10cd0c289c77702da221376f2a828baf7fc7b2
Merline Belizaire <merline@noukod.com>
2018-09-27 17:10:38 +0000

    automatically backed up by learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,6 +1,6 @@
 def my_hash
   # use the literal constructor to set the variable, my_hash, equal to a hash with key/value pairs of your choice.
-hash = {"my_name" => "Herman Melville"}
+hash = {"my_name" => "Merline"}
 
 end
 
